ac7e02f8f5933f78a4aa42fe62c797d176bc86b0
nimbx12 <dwinurcahyo955@gmail.com>
2023-05-30 21:12:29 +0700

    Initial commit

---
+++ app/src/test/java/com/example/utspraktik_akbif10_10122914_dwinurcahyo/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.utspraktik_akbif10_10122914_dwinurcahyo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
